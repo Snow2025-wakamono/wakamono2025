80dd79660101b5e791733464c3c2cee6c45a64dd
Snow2025-wakamono <snow1132labo@gmail.com>
2026-02-19 03:35:26 +0900

    snow

---
+++ assets/js/mypage.js
@@ -0,0 +1,63 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbx0n0J_k5N97VB2swa4k_8ZtoVwaH8twd9gCkDtFuXUIuQOLD-NcSoKg-4skHcmHbjVvA/exec";
+
+const loginBtn = document.getElementById("loginBtn");
+const loginId = document.getElementById("loginId");
+const loginError = document.getElementById("loginError");
+
+loginBtn.onclick = async () => {
+
+  loginError.textContent = "";
+  const id = loginId.value.trim();
+
+  if (!id) {
+    loginError.textContent = "IDを入力してください";
+    return;
+  }
+
+  const data = await fetchUser(id);
+
+  if (data.error) {
+    loginError.textContent = "IDが見つかりません";
+    return;
+  }
+
+  // 表示切替
+  document.getElementById("loginBox").hidden = true;
+  document.getElementById("mypageContent").hidden = false;
+
+  // 書き込み
+  set("nickname", data.nickname);
+  set("branch", data.branch);
+  set("grade", data.grade);
+  set("bus", data.bus);
+
+  set("freeAssign", data.freeAssign);
+  set("themeCourse", data.themeCourse);
+  set("themeGroup", data.themeGroup);
+
+  set("freeRoom", data.freeRoom);
+  set("themeRoom", data.themeRoom);
+  set("stayRoom", data.stayRoom);
+};
+
+document.getElementById("logoutBtn").onclick = () => location.reload();
+
+function set(id, value){
+  document.getElementById(id).textContent = value || "—";
+}
+
+/* JSONP通信 */
+function fetchUser(id){
+  return new Promise(resolve=>{
+    const cb = "cb_"+Date.now();
+    window[cb] = data=>{
+      resolve(data);
+      script.remove();
+      delete window[cb];
+    };
+
+    const script=document.createElement("script");
+    script.src=`${API_URL}?id=${id}&callback=${cb}`;
+    document.body.appendChild(script);
+  });
+}
